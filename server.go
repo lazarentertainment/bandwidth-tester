94bd7df707c944d984e66c09dbc9d8ff22e506e0
batasrki <spejic@gmail.com>
2014-12-30 14:37:25 -0500

    Set up CORS headers, so that I can hit the service from another URL

--- server.go
+++ server.go
@@ -10,15 +10,35 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/bandwidth", BandwidthHandler)
+	http.Handle("/", &LeServer{r})
 	log.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+type LeServer struct {
+	r *mux.Router
+}
+
+func (serv *LeServer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if origin := request.Header.Get("Origin"); origin != "" {
+		response.Header().Set("Access-Control-Allow-Origin", origin)
+		response.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		response.Header().Set("Access-Control-Allow-Headers",
+			"Accept, contentType, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	if request.Method == "OPTIONS" {
+		return
+	}
+	serv.r.ServeHTTP(response, request)
 }
 
 func BandwidthHandler(response http.ResponseWriter, request *http.Request) {
 	start := time.Now()
 	if request.Method == "POST" {
-		params := mux.Vars(request)
-		log.Println(params["image"])
+		params := request.ParseForm()
+		log.Println(request)
+		log.Println(params)
+		//log.Println(params["image"])
 		response.Write([]byte("This is a server response"))
 	} else {
 		http.Error(response, "Can't touch this", http.StatusUnauthorized)
